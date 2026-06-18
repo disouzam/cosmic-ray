79cd3c213ca9e31ed83d0d0d44c2ce2a2a974bcf
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-06-18 15:29:42 -0300

    Added test that checks behavior of private function `_make_diff` indirectly to ensure diff has a similar appearance as `git diff` command

--- tests/unittests/test_mutate_and_test.py
+++ tests/unittests/test_mutate_and_test.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 from cosmic_ray.mutating import mutate_and_test
-from cosmic_ray.work_item import MutationSpec, WorkResult, WorkerOutcome
+from cosmic_ray.work_item import MutationSpec, TestOutcome, WorkResult, WorkerOutcome
 
 
 def test_no_test_return_value(path_utils, data_dir):
@@ -28,5 +28,34 @@ def test_no_test_return_value(path_utils, data_dir):
             test_outcome=None,
             diff=None,
             worker_outcome=WorkerOutcome.NO_TEST,
+        )
+        assert result == expected
+
+
+def test_private_make_diff(path_utils):
+    current_path = Path.cwd()
+    example_folder = current_path.joinpath("tests/resources/example_project/adam")
+
+    with path_utils.excursion(example_folder):
+        result = mutate_and_test(
+            [
+                MutationSpec(
+                    Path("adam_1.py"),
+                    "core/ReplaceTrueWithFalse",
+                    1,
+                    # TODO: As in other places, these are placeholder position values. How can we not have to provide them?
+                    (0, 0),
+                    (0, 1),
+                )
+            ],
+            "python -m pytest tests",
+            1000,
+        )
+
+        expected = WorkResult(
+            output='',
+            test_outcome=TestOutcome.KILLED,
+            diff='--- mutation diff ---\n--- a/adam_1.py\n+++ b/adam_1.py\n@@ -33,7 +33,7 @@\n \n def bool_if():\n     if object():\n-        return True\n+        return False\n \n     raise Exception("bool_if() failed")\n ',
+            worker_outcome=WorkerOutcome.NORMAL,
         )
         assert result == expected
